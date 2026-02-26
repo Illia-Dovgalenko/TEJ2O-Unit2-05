59e35fb416f4f7d7199bac7099b06b197f127dde
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-02-26 06:29:46 +0200

    fff

--- main.ts
+++ main.ts
@@ -4,20 +4,18 @@
  * Created on: Feb 2026
  * This program: Temperature Kelvin
 */
+let temperatureCelsius: number = 0;
+let temperatureKelvin: number = 0;
 
-let temperatureCelsius: number
-let temperatureKelvin: number
-
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
+basic.clearScreen();
+basic.showIcon(IconNames.Happy);
 
 input.onButtonPressed(Button.B, function () {
-    temperatureCelsius = input.temperature()
-    temperatureKelvin = temperatureCelsius + 273.15
-    temperatureKelvin = Math.round(temperatureKelvin)
+    temperatureCelsius = input.temperature();
+    temperatureKelvin = Math.round(temperatureCelsius + 273.15);
 
-    basic.clearScreen()
-    basic.showString("The temperature is: " + temperatureKelvin.toString() + " K")
-})
+    basic.clearScreen();
+    basic.showString("The temperature is: " + temperatureKelvin + " K");
+});
  
 
